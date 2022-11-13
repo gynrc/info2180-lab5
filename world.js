0c9c565237c59efd75b62153c3cce2a91146deb6
Condoleezza Gaynor <condeleezagaynor@gmail.com>
2022-11-13 14:03:22 -0500

    modified files and added image

---
+++ world.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    var lookupBtn = document.querySelector('#lookup');
+    var cityBtn = document.querySelector('#city');
+
+    lookupBtn.addEventListener('click', searchCountry);
+    cityBtn.addEventListener('click', searchCountry);
+
+    function searchCountry () {
+        //console.log('button clicked');
+        var lookupVal = document.querySelector('#country').value;
+        //var cityVal = document.querySelector('#country').value;
+        fetch('http://localhost/info2180-lab5/world.php?query='+lookupVal)
+            .then(response => response.text())
+            .then(data => {
+                let result = document.querySelector('#result');
+                result.innerHTML = data;
+            })
+            .catch(error => console.log(error));
+    }
+
+}
--- world.php
+++ world.php
@@ -1,17 +1,71 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$country = $_GET['query'];
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (empty($country)) {
+  $stmt = $conn->query("SELECT * FROM countries");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  show($results);
+} elseif (!empty($country) /*&& smn about country here */) {
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  show($results);
+} else /*&& smn about city here */ {
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  showCity($results);
+}
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+
+<?php 
+  function show($country) {
+    echo "<table>"; 
+    echo "<thead>";
+      echo "<tr>";
+        echo "<th>Name</th>";
+        echo "<th>Continent</th>";
+        echo "<th>Independence</th>";
+        echo "<th>Head of State</th>";
+      echo "</tr>";
+    echo "</thead>";
+    echo"<tbody>";
+    foreach ($results as $row) {
+      echo "<tr>";
+        echo "<td> {$row['name']} </td>";
+        echo "<td> {$row['continent']} </td>";
+        echo "<td> {$row['independence_year']} </td>";
+        echo "<td> {$row['head_of_state']} </td>";
+      echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+  }
+
+  function showCity($city) {
+    echo "<table>"; 
+    echo "<thead>";
+      echo "<tr>";
+        echo "<th>Name</th>";
+        echo "<th>District</th>";
+        echo "<th>Population</th>";
+      echo "</tr>";
+    echo "</thead>";
+    echo"<tbody>";
+    foreach ($results as $row) {
+      echo "<tr>";
+        echo "<td> {$row['name']} </td>";
+        echo "<td> {$row['district']} </td>";
+        echo "<td> {$row['population']} </td>";
+      echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+  }
+?>
